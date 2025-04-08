5f2471e42415a3254a3e97f3efe82b7a84e004e6
Jessica Sweeney <158358705+jesweeney@users.noreply.github.com>
2025-04-07 18:13:26 -0700

    Add files via upload

---
+++ DiceMenu.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class DiceMenu {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        while (true) {
+            System.out.println("\n--- Dice Menu ---");
+            System.out.println("1. Roll two 6-sided dice");
+            System.out.println("2. Flip N 2-sided dice");
+            System.out.println("3. Exit");
+            System.out.print("Choose an option (1-3): ");
+            int choice = scanner.nextInt();
+
+            if (choice == 1) {
+                // Roll two 6-sided dice
+                int die1 = random.nextInt(6) + 1;
+                int die2 = random.nextInt(6) + 1;
+                System.out.println("You rolled: " + die1 + " and " + die2);
+            } else if (choice == 2) {
+                // Flip N 2-sided dice
+                System.out.print("Enter the number of 2-sided dice to flip: ");
+                int n = scanner.nextInt();
+
+                int headsCount = 0;
+                int tailsCount = 0;
+
+                System.out.println("Flipping " + n + " dice:");
+                for (int i = 1; i <= n; i++) {
+                    boolean isHeads = random.nextInt(2) == 0;
+                    if (isHeads) {
+                        System.out.println("Die " + i + ": Heads");
+                        headsCount++;
+                    } else {
+                        System.out.println("Die " + i + ": Tails");
+                        tailsCount++;
+                    }
+                }
+
+                System.out.println("Total Heads: " + headsCount);
+                System.out.println("Total Tails: " + tailsCount);
+
+            } else if (choice == 3) {
+                System.out.println("Goodbye!");
+                break;
+            } else {
+                System.out.println("Invalid option. Please try again.");
+            }
+        }
+
+        scanner.close();
+    }
+}
+
+
